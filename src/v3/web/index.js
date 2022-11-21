1c92160a7dc69068f5dd90ff9adf8811f210d916
Lucas Cazeto <lucas.cazeto@parcellab.com>
2022-11-21 18:20:27 +0100

    chore: add web components - 1st iteration

---
+++ src/v3/web/index.js
@@ -0,0 +1,26 @@
+class TrackAndTrace extends HTMLElement {
+  constructor() {
+    super();
+    this._options = window.parcelLabTrackAndTraceOptions || {};
+
+    const wrapper = document.createElement("div");
+    wrapper.setAttribute("id", "parcellab-track-and-trace");
+    this.appendChild(wrapper);
+
+    var linkTag = document.createElement("link");
+    linkTag.rel = "stylesheet";
+    linkTag.href = "https://cdn.parcellab.com/css/v3/parcelLab.min.css";
+    document.getElementsByTagName("head")[0].appendChild(linkTag);
+
+    const script = document.createElement("script");
+    script.src = "https://cdn.parcellab.com/js/v3/parcelLab.min.js";
+    script.async = true;
+    script.onload = () => {
+      window._prcl = new window.ParcelLab("#parcellab-track-and-trace", this._options);
+      window._prcl.initialize();
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+  }
+}
+
+customElements.define("track-and-trace", TrackAndTrace);
