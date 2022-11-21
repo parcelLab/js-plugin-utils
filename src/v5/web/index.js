3dd1aac6f744093b12747d44050f2b68734d6dcc
Lucas Cazeto <lucas.cazeto@parcellab.com>
2022-11-21 18:27:11 +0100

    chore: add disable styles

--- src/v5/web/index.js
+++ src/v5/web/index.js
@@ -2,15 +2,18 @@ class TrackAndTrace extends HTMLElement {
   constructor() {
     super();
     this._options = window.parcelLabTrackAndTraceOptions || {};
+    this._disableDefaultStyles = window.disableDefaultStyles || false;
 
     const wrapper = document.createElement("div");
     wrapper.setAttribute("id", "parcellab-track-and-trace");
     this.appendChild(wrapper);
 
-    var linkTag = document.createElement("link");
-    linkTag.rel = "stylesheet";
-    linkTag.href = "https://cdn.parcellab.com/css/v5/main.min.css";
-    document.getElementsByTagName("head")[0].appendChild(linkTag);
+    if (!this._disableDefaultStyles) {
+      var linkTag = document.createElement("link");
+      linkTag.rel = "stylesheet";
+      linkTag.href = "https://cdn.parcellab.com/css/v5/main.min.css";
+      document.getElementsByTagName("head")[0].appendChild(linkTag);
+    }
 
     const script = document.createElement("script");
     script.src = "https://cdn.parcellab.com/js/v5/main.min.js";
